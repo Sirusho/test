b80f3094593da56a3ebebd60188ac3e141c9ca69
Siranush Kamavosyan <sirushokamavosyan@gmail.com>
2021-04-26 18:07:26 +0400

    astvac jan

---
+++ src/com/company/Circle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Circle {
+
+    public static void main(String[] args) {
+        double radius=3;
+        double pi=3.14;
+        double area=pi*radius*radius;
+        double perimeter=2*pi*radius;
+
+        System.out.println("Area is -> " +  area);
+        System.out.println("Perimeter is -> " + perimeter);
+    }
+}
+
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        int c=5;
+        int area=a*b/2;
+        int perimeter=(a+b)*2;
+        System.out.println("area is -> " + area);
+        System.out.println("perimeter is -> " + perimeter);
+    }
+}
